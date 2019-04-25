7f327f3208fe8be8341a3b9aaad492537fa85a9e
sophia <sophierithous@gmail.com>
2019-04-25 16:04:40 +0100

    added functionality

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
+//jshint esversion:6
 $(document).ready(function () {
+
+    $('.new-quote').click(function () {
+
     const URL = 'https://api.forismatic.com/api/1.0/?format=jsonp&method=getQuote&jsonp=?&lang=en';
 
     $.getJSON(URL, function (data) {
-        console.log(data);
+
+      let quote = data.quoteText;
+      let author = data.quoteAuthor;
+
+      $('#quote').text(quote);
+
+      if (author === '') {
+            $('#author').text('~ Unknown');
+        } else {
+            $('#author').text('~'+ author);
+        }
+        console.log(author)
+
+
+
     });
 
+    });
 });
